a653ec5f96619f157775b8345b5b9c3f87cd0727
Yan Yang <y.y.erica@hotmail.com>
2025-09-01 16:05:42 +0200

    association table auskommentiert

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 export { default as Order } from "./Order.js";
 export { default as User } from "./User.js";
 export { default as Product } from "./Product.js";
-export { default as OrderProduct } from "./OrderProduct.js";
+// export { default as OrderProduct } from "./OrderProduct.js";
 export { default as Category } from "./Category.js";
